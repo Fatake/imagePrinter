74b94dc36d339bc4869c5afd2b54d1f96cbbfe77
Fatake <pace_lozano@hotmail.com>
2020-11-24 13:11:48 -0600

    Cambiando cantidad de fotos en la imprecion

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import platform
 import wx
 import wx.xrc
 
-
 ###########################################################################
 ## Class PyPhoto
 ###########################################################################
@@ -16,17 +15,36 @@ class PyPhoto ( wx.Frame ):
 	def __init__( self, parent ):
 		self.WIDTH_PHOTO = 480
 		self.HEIGHT_PHOTO = 640
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"PyPhoto", pos = wx.DefaultPosition, size = wx.Size( 690,747 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"PyPhoto", pos = wx.DefaultPosition, size = wx.Size( 767,827 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
 		mainContainer = wx.BoxSizer( wx.VERTICAL )
-		self.locale = wx.Locale(wx.LANGUAGE_SPANISH_MEXICAN)
 
 		self.panelPrincipal = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
 		contenedor = wx.GridBagSizer( 0, 0 )
 		contenedor.SetFlexibleDirection( wx.BOTH )
 		contenedor.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+
+		gSizer1 = wx.GridSizer( 0, 1, 0, 0 )
+
+		self.m_staticText5 = wx.StaticText( self.panelPrincipal, wx.ID_ANY, u"Cantidad de Fotos en Hoja", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText5.Wrap( -1 )
+
+		gSizer1.Add( self.m_staticText5, 0, wx.ALL, 5 )
+
+		self.m_radioBtn1 = wx.RadioButton( self.panelPrincipal, wx.ID_ANY, u"4x4", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_radioBtn1.SetValue( True )
+		gSizer1.Add( self.m_radioBtn1, 0, wx.ALL, 5 )
+
+		self.m_radioBtn2 = wx.RadioButton( self.panelPrincipal, wx.ID_ANY, u"6x6", wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer1.Add( self.m_radioBtn2, 0, wx.ALL, 5 )
+
+		self.m_radioBtn3 = wx.RadioButton( self.panelPrincipal, wx.ID_ANY, u"8x8", wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer1.Add( self.m_radioBtn3, 0, wx.ALL, 5 )
+
+
+		contenedor.Add( gSizer1, wx.GBPosition( 0, 4 ), wx.GBSpan( 1, 1 ), wx.ALIGN_RIGHT|wx.RIGHT, 5 )
 
 		self.selectoImagen = wx.FilePickerCtrl( self.panelPrincipal, wx.ID_ANY, u"D:\\Documentos\\programaImagenes\\bob.jpg", u"Seleccione una foto", u"JPG files (*.jpg)|*.jpg|PNG files (*.png)|*.PNG|BMP files (*.bmp)|*.bmp", wx.DefaultPosition, wx.DefaultSize, wx.FLP_CHANGE_DIR|wx.FLP_FILE_MUST_EXIST|wx.FLP_OPEN )
 		contenedor.Add( self.selectoImagen, wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
@@ -62,9 +80,11 @@ class PyPhoto ( wx.Frame ):
 		self.Centre( wx.BOTH )
 
 		# Connect Events
+		self.m_radioBtn1.Bind( wx.EVT_RADIOBUTTON, self.change4 )
+		self.m_radioBtn2.Bind( wx.EVT_RADIOBUTTON, self.change6 )
+		self.m_radioBtn3.Bind( wx.EVT_RADIOBUTTON, self.change8 )
 		self.selectoImagen.Bind( wx.EVT_FILEPICKER_CHANGED, self.load )
 		self.Bind( wx.EVT_MENU, self.printPrinter, id = self.imprime4.GetId() )
-
 
 	# Virtual event handlers, overide them in your derived class
 	def load( self, event ):
@@ -82,10 +102,17 @@ class PyPhoto ( wx.Frame ):
 		self.imagenEditar.Bitmap = wx.Bitmap( name+".jpg", wx.BITMAP_TYPE_ANY )
 		self.panelPrincipal.Refresh()
 	
-	def printPrinter( self, event ):
+	def change4( self, event ):
 		event.Skip()
 
+	def change6( self, event ):
+		event.Skip()
 
+	def change8( self, event ):
+		event.Skip()
+
+	def printPrinter( self, event ):
+		event.Skip()
 
 ###########################################################################
 ## Main Program
